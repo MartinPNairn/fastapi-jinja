d803dc98faf1b0859895262aad4eb49e5a44efb8
Martin Pistolesi Nairn <martin_pisto@hotmail.com>
2026-06-18 11:24:02 +0200

    tests: update test_users_endpoints

--- tests/test_users/test_users_endpoints.py
+++ tests/test_users/test_users_endpoints.py
@@ -1,12 +1,8 @@
-from app.crud import get_entry
-from app.models import User
 from app.schemas import UserResponse
-from app.core.security.password_hasher import verify_password_hash
+from app.core.security.password_hasher import PwdlibPasswordHasher
 
 
-# TODO: UPDATE THESE TESTS TO USE USER SERVICE
-
-def test_create_user(client, session):
+def test_create_user(client, user_service):
     test_client = client()
     new_user = {
         'email': 'john@gmail.com',
@@ -19,17 +15,16 @@ def test_create_user(client, session):
     }
     response = test_client.post("/users/create", json=new_user)
     assert response.status_code == 201
-    user = get_entry(User, session, User.username == new_user["username"])
-    assert user is not None
+    user = user_service.get_by_username(new_user["username"])
+    assert user.email == new_user["email"]
 
 
 def test_get_user(client, test_user):
     test_client = client(test_user)
     response = test_client.get("/users/get-user")
     assert response.status_code == 200
-    for attr, value in UserResponse.model_validate(test_user).model_dump(mode="json").items():
-        assert response.json()[attr] == value
-
+    assert response.json() == UserResponse.model_validate(test_user).model_dump(mode="json")
+   
 
 def test_update_password(client, test_user, session):
     test_client = client(test_user)
@@ -40,7 +35,7 @@ def test_update_password(client, test_user, session):
     response = test_client.put("/users/update-password", json=request_data)
     assert response.status_code == 204
     session.refresh(test_user)
-    assert verify_password_hash(request_data["new_password"], test_user.hashed_password)
+    assert PwdlibPasswordHasher().verify_hash(request_data["new_password"], test_user.hashed_password)
 
 
 def test_update_password_invalid_current_password(client, test_user):
